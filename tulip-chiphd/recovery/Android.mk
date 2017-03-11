5194a8a1c41136b0f89147eb5aa424c1ea22972f
Kamil Trzcinski <ayufan@ayufan.eu>
2017-03-12 00:54:54 +0100

    Fix recovery Android.mk (backport)

--- tulip-chiphd/recovery/Android.mk
+++ tulip-chiphd/recovery/Android.mk
@@ -1,14 +1,14 @@
-ifneq (,$(findstring $(TARGET_DEVICE),tulip-p1))
+ifneq (,$(findstring $(TARGET_DEVICE),tulip-chiphd))
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_CLANG := true
-LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := aw_ui.cpp
+LOCAL_SRC_FILES := default_device.cpp
 
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
-LOCAL_MODULE := librecovery_ui_tulip_p1
+LOCAL_MODULE := librecovery_ui_tulip_chiphd
 
 include $(BUILD_STATIC_LIBRARY)
 
