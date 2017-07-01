8d79fdd769ffaebac34276d19f4ff1a2d3331a88
Kamil Trzcinski <ayufan@ayufan.eu>
2017-07-01 12:00:44 +0200

    Remove apps

--- common/prebuild/preinstallapk/SideloadLauncher/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := SideloadLauncher
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := SideloadLauncher.apk
-include $(BUILD_PREBUILT)
